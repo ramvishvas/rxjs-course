9964f932f4639719111deef3b7dc23309f9ea432
ram <ramvishvas.kumar@primathon.in>
2020-01-22 10:27:37 +0530

    [ADD] resoverable error handler

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,32 +27,28 @@ export class HomeComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    // PART 1
-    // const http$ = createHttpObserable("/api/courses");
-
-    // const courses$ = http$.pipe(map(res => Object.values(res["payload"])));
-
-    // courses$.subscribe(
-    //   courses => {
-    //     console.log(courses);
-    //     this.beginnerCourses = courses.filter(
-    //       course => course.category == "BEGINNER"
-    //     );
-    //     this.advanceCourses = courses.filter(
-    //       course => course.category == "ADVANCED"
-    //     );
-    //   },
-    //   noop,
-    //   () => console.log("completed")
-    // );
-
-    // PART 2
     const http$: Observable<Course[]> = createHttpObserable("/api/courses");
 
     const courses$ = http$.pipe(
       tap(() => console.log("Http Request Executed")),
       map(res => Object.values(res["payload"])),
-      shareReplay()
+      shareReplay(),
+      catchError(err =>
+        of([
+          {
+            id: 0,
+            description: "RxJs In Practice Course",
+            iconUrl:
+              "https://s3-us-west-1.amazonaws.com/angular-university/course-images/rxjs-in-practice-course.png",
+            courseListIcon:
+              "https://angular-academy.s3.amazonaws.com/main-logo/main-page-logo-small-hat.png",
+            longDescription:
+              "Understand the RxJs Observable pattern, learn the RxJs Operators via practical examples",
+            category: "BEGINNER",
+            lessonsCount: 10
+          }
+        ])
+      )
     );
 
     this.beginnerCourses$ = courses$.pipe(
